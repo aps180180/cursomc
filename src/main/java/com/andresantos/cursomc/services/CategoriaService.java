4f7b2412af833ca4ac0e65b4735124abd84ab023
aps180180 <aps180180@gmail.com>
2018-09-01 11:48:56 -0300

    Ajustes no endpoint/categorias/{id}

--- src/main/java/com/andresantos/cursomc/services/CategoriaService.java
+++ src/main/java/com/andresantos/cursomc/services/CategoriaService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 
 import com.andresantos.cursomc.domain.Categoria;
 import com.andresantos.cursomc.repositories.CategoriaRepository;
+import com.andresantos.cursomc.services.exceptions.ObjectNotFoundException;
+
+
 
 @Service
 public class CategoriaService {
@@ -16,6 +19,7 @@ public class CategoriaService {
 	
 	public Categoria find( Integer id) {
 		Optional <Categoria> obj = repo.findById(id);
-		return obj.orElse (null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())); 
 	}
 }
