dbbb08cfaf495afcc5f1addec28c4be9753d9d07
aps180180 <aps180180@gmail.com>
2018-09-01 15:41:53 -0300

    Estado e Cidade

---
+++ src/main/java/com/andresantos/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,12 @@
+package com.andresantos.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.andresantos.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado,Integer>{
+
+	
+}
