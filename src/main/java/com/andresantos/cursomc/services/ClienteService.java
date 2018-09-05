9e156cef54ec56117f99c28b8a70bef861953d5e
aps180180 <aps180180@gmail.com>
2018-09-05 16:41:55 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/andresantos/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.andresantos.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andresantos.cursomc.domain.Cliente;
+import com.andresantos.cursomc.repositories.ClienteRepository;
+import com.andresantos.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente find( Integer id) {
+		Optional <Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName())); 
+	}
+}
