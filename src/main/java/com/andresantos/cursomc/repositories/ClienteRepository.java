9b96ca103a6844364027f5a6bb5a9a29e88050ae
aps180180 <aps180180@gmail.com>
2018-09-22 19:25:04 -0300

    Validação customizada - Email não repetido no cadastro de Cliente

--- src/main/java/com/andresantos/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/andresantos/cursomc/repositories/ClienteRepository.java
@@ -1,12 +1,18 @@
 package com.andresantos.cursomc.repositories;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.andresantos.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente,Integer>{
-
+	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
+	
+	// aqui no repository se declarar o metodo acima, o spring gera um metodo de busca findBy<campo>
 	
 }
