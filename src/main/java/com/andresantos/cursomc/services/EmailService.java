cd4546058037af49e35e3ecc913a5161a2a8d264
aps180180 <aps180180@gmail.com>
2018-09-26 23:17:45 -0300

    Email HTML

--- src/main/java/com/andresantos/cursomc/services/EmailService.java
+++ src/main/java/com/andresantos/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.andresantos.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.andresantos.cursomc.domain.Pedido;
@@ -9,4 +11,7 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	void sendMail(SimpleMailMessage msg);
 	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	void sendHtmlEmail(MimeMessage msg);
+		
 }
